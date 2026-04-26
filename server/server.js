8b8cfb7eb36474c635e661a6ceb459697e869ad1
Sanchali <sanchali280705@gmail.com>
2026-04-26 17:08:35 +0530

    debug phase done

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const sequilize = require("./config/db");
+const sequelize = require("./config/db");
 //import models 
 require("./models");
 const authRoutes = require("./routes/authRoutes");
