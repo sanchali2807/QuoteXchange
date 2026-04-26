92c53a9d95d16bb33247896b85e71b31e0a8018a
Sanchali <sanchali280705@gmail.com>
2026-04-26 17:34:53 +0530

    debug phase done

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,11 @@
-const {Sequelize} = require("sequelize");
-require("dotenv").config(); // Loads .env variables into process.env
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "mysql",
+  timezone: "+05:30",
+  logging: false
+});
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host : process.env.DB_HOST,
-        port : process.env.DB_PORT,
-        dialect : "mysql",
-        timezone: "+05:30",
-    //     dialectOptions: {
-    //     useUTC: false
-    // },
-        logging : false
-    }
-);
 module.exports = sequelize;
+
