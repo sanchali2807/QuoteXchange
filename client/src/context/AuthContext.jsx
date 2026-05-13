8b0c6875fa6f6d3d00d7c1b1c70fd6319bc66262
Sanchali <sanchali280705@gmail.com>
2026-05-13 21:08:09 +0530

    render commit'

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
-
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(
     localStorage.getItem("token") || null
