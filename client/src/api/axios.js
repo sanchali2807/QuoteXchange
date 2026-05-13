f51e07bb0c6a10d9024b559b01c5c8da2ceb537b
Sanchali <sanchali280705@gmail.com>
2026-05-13 20:58:54 +0530

    render commit'

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -19,7 +19,7 @@ api.interceptors.request.use((config)=>{
 api.interceptors.response.use((res)=>res,(err)=>{
     if(err.response?.status === 401){
         localStorage.removeItem("token");
-        window.location.href("./login")
+        window.location.href("/login")
     }
     return Promise.reject(error);
 })
