c36906a8e784417a40c344fc595ec80e1373e7c1
Sanchali <sanchali280705@gmail.com>
2026-05-14 17:57:14 +0530

    render commit'

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: "mysql",
   logging: false,
-  // timezone: "+05:30",
-  // dialectOptions: {
-  //   dateStrings: true,
-  //   typeCast: true
-  // }
+  timezone: "+05:30",
+  dialectOptions: {
+    dateStrings: true,
+    typeCast: true
+  }
 });
 module.exports = sequelize;
 
